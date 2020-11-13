1459568291743e612ec5dedaff0a8df84d71f6b1
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-13 10:04:52 +0700

    PBOIF2-10119082-Latihan42-Tabungan

---
+++ src/pboif2/pkg10119082/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan42.tabungan;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Tabungan
+ */
+public class Tabungan {
+    private int saldo;
+
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return saldo - jumlah;
+    }
+}
